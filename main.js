ae570e9124b353de50c70c9b1ab5a38d36d4ccb6
sgujuluva <sanghee04@gmail.com>
2022-06-05 11:42:09 +0200

    counter increment selected in js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 let selectText = document.querySelector(".text");
 let imageContainer = document.querySelector(".container");
 let load = 0;
+let int = setInterval(increaseCounter,30);
+
+function increaseCounter(){
+    load ++;   
+    if(load > 99){
+        clearInterval(int)
+    }
+}
